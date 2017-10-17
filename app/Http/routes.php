e6a765a1b385aecb9c9c98c185c77d31cc493297
www846www <e0981298711@gmail.com>
2017-10-17 13:07:48 +0800

    修改route成選擇性參數

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{name}', function ($name) {
+Route::get('hello/{name?}', function ($name = 'Everybody') {
     return 'Hello, '.$name;
 });
